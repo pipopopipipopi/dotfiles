6c2ac21e6700d9c579e75c682c23fdf5a4361043
pipopo <puchi.maru.milk@gmail.com>
2024-10-04 21:18:34 +0900

    edit darwin shell settings

--- hosts/aura/darwin.nix
+++ hosts/aura/darwin.nix
@@ -8,6 +8,7 @@
     ../../modules/darwin/core
     ../../modules/darwin/desktop/fonts.nix
     ../../modules/darwin/programs/darwin-system.nix
+    ../../modules/darwin/programs/shell.nix
     ../../modules/darwin/programs/homebrew.nix
   ];
 
--- modules/darwin/programs/shell.nix
+++ modules/darwin/programs/shell.nix
@@ -2,11 +2,5 @@
   programs = {
     zsh.enable = true;
     fish.enable = true;
-    git.enable = true;
-    neovim = {
-      enable = true;
-      defaultEditor = true;
-      vimAlias = true;
-    };
   };
 }
