ca70e20ff12e0d15d6d16e890ca6c9db11b2dabf
pipopo <puchi.maru.milk@gmail.com>
2024-10-14 15:42:54 +0900

    remove vivaldi

--- home-manager/nixos/browser/default.nix
+++ home-manager/nixos/browser/default.nix
@@ -1,14 +1,4 @@
 { pkgs, inputs, ... }:
 {
   home.packages = [ inputs.zen-browser.packages.${pkgs.system}.default ];
-
-  programs = {
-    vivaldi = {
-      enable = true;
-      commandLineArgs = [
-        "--enable-features=WebUIDarkMode"
-        "--force-dark-mode"
-      ];
-    };
-  };
 }
