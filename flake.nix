9c32b3e018147c3c59ea822d5f78d490310cfb34
pipopo <puchi.maru.milk@gmail.com>
2024-12-28 20:27:00 +0900

    install ghostty

--- flake.nix
+++ flake.nix
@@ -53,6 +53,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    ghostty.url = "github:ghostty-org/ghostty";
+
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
---
+++ home/common/gui/ghostty/default.nix
@@ -0,0 +1,5 @@
+{ inputs, pkgs, ... }: {
+  home.packages = [
+    inputs.ghostty.packages.${pkgs.system}.default
+  ];
+}
