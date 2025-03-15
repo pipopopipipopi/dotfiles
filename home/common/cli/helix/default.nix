110c4ccb87d972b29709087e94191f5df7a03380
pipopo <puchi.maru.milk@gmail.com>
2025-03-16 00:13:22 +0900

    edit helix settings

--- home/common/cli/helix/default.nix
+++ home/common/cli/helix/default.nix
@@ -2,5 +2,12 @@
   programs.helix = {
     enable = true;
     settings = (builtins.fromTOML (builtins.readFile ./config.toml));
+    themes = {
+      rose_pine_moon_transparent = {
+        "inherits" = "rose_pine_moon";
+        "ui.background" = {};
+        "ui.background.separator" = {};
+      };
+    };
   };
 }
