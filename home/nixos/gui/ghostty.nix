f12a2e676abb1327966694d8d08b113ce19df358
pipopo <puchi.maru.milk@gmail.com>
2025-03-21 16:51:42 +0900

    edit ghostty settings

--- home/nixos/gui/ghostty.nix
+++ home/nixos/gui/ghostty.nix
@@ -10,7 +10,7 @@
       theme = "amewotaguru";
 
       # font-family = ["JetBrainsMono Nerd Font Mono" "HackGen Console NF" "Twitter Color Emoji"];
-      font-family = ["UDEV Gothic NF Regular" "Twitter Color Emoji"];
+      font-family = ["" "UDEV Gothic NF Regular" "Twitter Color Emoji"];
       font-style = "Medium";
       font-size = 12;
       font-feature = ["-liga" "-dlig" "-calt"];
