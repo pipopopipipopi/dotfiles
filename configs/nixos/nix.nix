835c9e4613a8d1fa64e276e78a076a3dd8302aff
pipopo <puchi.maru.milk@gmail.com>
2025-04-23 17:36:06 +0900

    add helix substituters

--- configs/nixos/nix.nix
+++ configs/nixos/nix.nix
@@ -21,11 +21,13 @@
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
         "https://niri.cachix.org"
+        "https://helix.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+        "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
       ];
     };
   };
