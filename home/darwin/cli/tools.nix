53544438a20b880164b752e19ef2f3e96232d264
pipopo <puchi.maru.milk@gmail.com>
2025-05-01 19:57:47 +0900

    update brew-nix and install desktoppr

--- home/darwin/cli/tools.nix
+++ home/darwin/cli/tools.nix
@@ -1,5 +1,14 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  desktoppr = pkgs.brewCasks.desktoppr.overrideAttrs (_: {
+    installPhase = ''
+      mkdir -p $out/bin
+      cp ./usr/local/bin/desktoppr $out/bin/
+    '';
+  });
+in {
   home.packages = with pkgs; [
     colima
+    desktoppr
   ];
 }
