3716e09f7e2f01ff680584df3f8b5ec9ca810360
pipopo <puchi.maru.milk@gmail.com>
2025-05-01 16:48:49 +0900

    edit matugen settings

--- home/common/cli/matugen/default.nix
+++ home/common/cli/matugen/default.nix
@@ -1,5 +1,10 @@
-{
-  xdg.configFile."matugen/config.toml".source = ./config.toml;
+{ pkgs, ... }: {
+  
+  xdg.configFile."matugen/config.toml".source =
+    if pkgs.stdenv.isLinux then
+      ./nixos_config.toml
+    else
+      ./darwin_config.toml;
   xdg.configFile."matugen/templates/wezterm.toml".source = ./wezterm.toml;
   xdg.configFile."matugen/templates/fish.theme".source = ./fish.theme;
 }
