ed3cfef64c33c8b6fec6bb7ced6fa61da47ce266
pipopo <puchi.maru.milk@gmail.com>
2025-05-08 22:20:50 +0900

    add matugen templates for yazi

--- home/common/cli/yazi/default.nix
+++ home/common/cli/yazi/default.nix
@@ -1,5 +1,7 @@
 {
   programs.yazi = {
     enable = true;
+    settings = (builtins.fromTOML (builtins.readFile ./yazi.toml));
+    keymap = (builtins.fromTOML (builtins.readFile ./keymap.toml));
   };
 }
