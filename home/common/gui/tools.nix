8bccd37d93f40bbb7e70821d55f87cd6276541da
pipopo <puchi.maru.milk@gmail.com>
2025-05-17 10:36:46 +0900

    edit vesktop settings

--- home/common/gui/tools.nix
+++ home/common/gui/tools.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     anki-bin
-    vesktop
   ];
 }
--- home/common/gui/vesktop.nix
+++ home/common/gui/vesktop.nix
@@ -1,1 +1,52 @@
-{}
+{
+  programs.vesktop = {
+    enable = true;
+
+    settings = {
+      discordBranch = "stable";
+    };
+
+    vencord = {
+      settings = {
+        autoUpdate = false;
+        autoUpdateNotification = false;
+
+        disableMinSize = true;
+
+        plugins = {
+          CrashHandler = {
+            enabled = true;
+          };
+          CtrlEnterSend = {
+            enabled = true;
+          };
+          FakeNitro = {
+            enabled = true;
+          };
+          WebKeyBinds = {
+            enabled = true;
+          };
+          WebScreenShareFixes = {
+            enabled = true;
+          };
+          DisableDeepLinks = {
+            enabled = true;
+          };
+          NoTrack = {
+            enabled = true;
+          };
+          Settings = {
+            enabled = true;
+            settingsLocation = "aboveNitoro";
+          };
+          SupporHelper = {
+            enabled = true;
+          };
+          WebContexMenus = {
+            enabled = true;
+          };
+        };
+      };
+    };
+  };
+}
