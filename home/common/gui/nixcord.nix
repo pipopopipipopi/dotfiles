b2e01ed6f4a08fb3a7644f022812b71c03b1e2fe
pipopo <puchi.maru.milk@gmail.com>
2025-06-14 16:59:33 +0900

    edit nixcord settings

--- home/common/gui/nixcord.nix
+++ home/common/gui/nixcord.nix
@@ -3,7 +3,33 @@
 
   programs.nixcord = {
     enable = true;
+    discord.enable = false;
     vesktop.enable = true;
-    config = {};
+
+    config = {
+      autoUpdate = false;
+      autoUpdateNotification = false;
+      useQuickCss = true;
+      frameless = true;
+      transparent = true;
+      disableMinSize = true;
+
+      plugins = {
+        callTimer.enable = true;
+        clearURLs.enable = true;
+        crashHandler.enable = true;
+        ctrlEnterSend = {
+          enable = true;
+          submitRule = "ctrl+enter";
+          sendMessageInTheMiddleOfACodeBlock = true;
+        };
+        fakeNitro.enable = true;
+        gifPaste.enable = true;
+        memberCount.enable = true;
+        platformIndicators.enable = true;
+        volumeBooster.enable = true;
+        whoReacted.enable = true;
+      };
+    };
   };
 }
