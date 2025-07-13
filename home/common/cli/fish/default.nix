df9b3048f7d6d43478f1c3b7e3045920389a851f
pipopo <puchi.maru.milk@gmail.com>
2025-07-13 11:38:48 +0900

    edit fish settings

--- home/common/cli/fish/default.nix
+++ home/common/cli/fish/default.nix
@@ -4,7 +4,6 @@
     enable = true;
 
     interactiveShellInit = builtins.readFile ./config.fish;
-    # shellInit = builtins.readFile ./theme.fish;
 
     shellAliases = import ./aliases.nix;
   };
