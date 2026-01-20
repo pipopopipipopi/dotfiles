c425b49d7a7f6d4a64d852ace561f3b8c829b8fa
pipopo <puchi.maru.milk@gmail.com>
2026-01-20 15:48:06 +0900

    edit mako settings

--- home/nixos/desktop/mako.nix
+++ home/nixos/desktop/mako.nix
@@ -5,6 +5,9 @@
       anchor = "top-right";
       font = "JetBrainsMono Nerd Font 10";
       icons = true;
+      border-radius = 10;
+      default-timeout = 3000;
+      margin = "10,10,5";
     };
     extraConfig = ''
       include=~/.config/mako/colors
