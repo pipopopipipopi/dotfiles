ea5e18359e9a1d4eae082823a9130fd69d47d177
pipopo <puchi.maru.milk@gmail.com>
2026-02-06 15:04:32 +0900

    edit color themes

--- home/themes/palettes/coffee.nix
+++ home/themes/palettes/coffee.nix
@@ -1,22 +1,22 @@
 {
   fg = "#3d130c";
   bg = "#f2d2c5";
-  sub_bg = "#e39c80";
+  sub_bg = "#e9a58b";
 
   black = "#300f09";
-  red = "#8c4032";
-  green = "#6b9a67";
-  yellow = "#9c5248";
-  blue = "#9d6f77";
+  red = "#87333d";
+  green = "#52864d";
+  yellow = "#985d46";
+  blue = "#8a4551";
   magenta = "#8c4954";
   cyan = "#6e4945";
   white = "#f3d6cb";
 
   bright_black = "#5d2216";
-  bright_red = "#8c4032";
-  bright_green = "#6b9a67";
-  bright_yellow = "#9c5248";
-  bright_blue = "#9d6f77";
+  bright_red = "#87333d";
+  bright_green = "#52864d";
+  bright_yellow = "#985d46";
+  bright_blue = "#8a4551";
   bright_magenta = "#8c4954";
   bright_cyan = "#6e4945";
   bright_white = "#f2d7cc";
