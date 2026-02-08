db3413db24d7295e8d264668f4c57616b2b30bea
pipopo <puchi.maru.milk@gmail.com>
2026-02-08 15:12:44 +0900

    edit theme

--- home/themes/palettes/washi.nix
+++ home/themes/palettes/washi.nix
@@ -1,7 +1,7 @@
 {
   fg = "#1c1c1c";
   bg = "#fcfaf2";
-  sub_bg = "#d9dc90";
+  sub_bg = "#d9cd90";
 
   black = "#080808";
   red = "#cb4042";
@@ -9,7 +9,7 @@
   yellow = "#9b6e23";
   blue = "#0d5661";
   magenta = "#8e354a";
-  cyan = "#0d5661";
+  cyan = "#838a2d";
   white = "#fffffb";
 
   bright_black = "#434343";
@@ -18,6 +18,6 @@
   bright_yellow = "#9b6e23";
   bright_blue = "#0d5661";
   bright_magenta = "#8e354a";
-  bright_cyan = "#0d5661";
+  bright_cyan = "#838a2d";
   bright_white = "#f2d7cc";
 }
