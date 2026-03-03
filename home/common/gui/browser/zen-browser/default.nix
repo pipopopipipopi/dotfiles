ff14865ead1a2772b2995c2b97f1b60201e544cb
pipopo <puchi.maru.milk@gmail.com>
2026-03-03 15:35:46 +0900

    default browser firefox -> zen

---
+++ home/common/gui/browser/zen-browser/default.nix
@@ -0,0 +1,84 @@
+{ inputs, ... }: {
+  imports = [
+    inputs.zen-browser.homeModules.twilight
+  ];
+
+  programs.zen-browser = {
+    enable = true;
+    suppressXdgMigrationWarning = true;
+    policies = {
+      DisableAppUpdate = true;
+      DisableTelemetry = true;
+      TranslateEnabled = false;
+    };
+    profiles."default" = {
+      isDefault = true;
+
+      search = {
+        force = true;
+        default = "ddg";
+        privateDefault = "ddg";
+        engines = {
+          google.metaData.alias = "gg";
+          ddg.metaData.alias = "dd";
+          bing.metaData.hidden = true;
+          wikipedia.metaData.hidden = true;
+          startpage = {
+            name = "Startpage";
+            urls = [{
+              template = "https://www.startpage.com/sp/search";
+              params = [
+                { name = "query"; value = "{searchTerms}"; }
+              ];
+            }];
+            definedAliases = [ "sp" ];
+          };
+          youtube = {
+            name = "YouTube";
+            urls = [{
+              template = "https://www.youtube.com/results";
+              params = [
+                { name = "search_query"; value = "{searchTerms}"; }
+              ];
+            }];
+            definedAliases = [ "yt" ];
+          };
+          github = {
+            name = "GitHub";
+            urls = [{
+              template = "https://github.com/search";
+              params = [
+                { name = "type"; value = "repositories"; }
+                { name = "q"; value = "{searchTerms}"; }
+              ];
+            }];
+            definedAliases = [ "gh" ];
+          };
+          nix-packages = {
+            name = "Nix Packages";
+            urls = [{
+              template = "https://search.nixos.org/packages";
+              params = [
+                { name = "channel"; value = "unstable"; }
+                { name = "type"; value = "packages"; }
+                { name = "query"; value = "{searchTerms}"; }
+              ];
+            }];
+            definedAliases = [ "nixpkgs" ];
+          };
+          home-manager = {
+            name = "Home Manager";
+            urls = [{
+              template = "https://home-manager-options.extranix.com";
+              params = [
+                { name = "release"; value = "master"; }
+                { name = "query"; value = "{searchTerms}"; }
+              ];
+            }];
+            definedAliases = [ "hm" ];
+          };
+        };
+      };
+    };
+  };
+}
--- home/nixos/gui/default.nix
+++ home/nixos/gui/default.nix
@@ -1,6 +1,6 @@
 {
   imports = [
-    ./browser
+    # ./browser
     ./tools.nix
   ];
 }
