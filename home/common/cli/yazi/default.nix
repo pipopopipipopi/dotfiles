19b967edaa8eb59163b59940fa219b74404ed1db
pipopo <puchi.maru.milk@gmail.com>
2026-04-12 20:12:51 +0900

    edit yazi settings

--- home/common/cli/yazi/default.nix
+++ home/common/cli/yazi/default.nix
@@ -4,6 +4,5 @@
     settings = builtins.fromTOML (builtins.readFile ./yazi.toml);
     keymap = builtins.fromTOML (builtins.readFile ./keymap.toml);
     theme = builtins.fromTOML (builtins.readFile ./theme.toml);
-    flavors = { rose-pine-moon = ./rose-pine-moon; };
   };
 }
