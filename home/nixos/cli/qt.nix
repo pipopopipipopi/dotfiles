c0816926bbd8c8daa9f50f92b4a707fe60585f71
pipopo <puchi.maru.milk@gmail.com>
2026-04-19 14:50:27 +0900

    kdeテーマを編集

--- home/nixos/cli/qt.nix
+++ home/nixos/cli/qt.nix
@@ -3,5 +3,11 @@
     enable = true;
     platformTheme.name = "qtct";
     style.name = "kvantum";
+
+    kvantum = {
+      enable = true;
+      themes = with pkgs; [ whitesur-kde ];
+      settings.General.theme = "WhiteSur";
+    };
   };
 }
