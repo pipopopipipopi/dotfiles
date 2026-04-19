9a64d2ce42057e83fe2dca5ea80d13169613a950
pipopo <puchi.maru.milk@gmail.com>
2026-04-19 12:35:15 +0900

    デフォルトアプリを編集

--- home/nixos/desktop/xdg.nix
+++ home/nixos/desktop/xdg.nix
@@ -1,31 +1,35 @@
 {
   xdg = {
     enable = true;
+    mimeApps.enable = true;
+    configFile."mimeapps.list".force = true;
     mimeApps.associations.added = {
-      "text/html" = [ "zen.desktop" ];
-      "x-scheme-handler/http" = [ "zen.desktop" ];
-      "x-scheme-handler/https" = [ "zen.desktop" ];
-      "image/png" = [ "org.gnome.Loupe.desktop" ];
-      "image/svg" = [ "org.gnome.Loupe.desktop" ];
-      "image/jpeg" = [ "org.gnome.Loupe.desktop" ];
-      "image/gif" = [ "org.gnome.Loupe.desktop" ];
-      "image/webp" = [ "org.gnome.Loupe.desktop" ];
-      "video/mp4" = [ "io.github.celluloid_player.Celluloid.desktop" ];
-      "video/avi" = [ "io.github.celluloid_player.Celluloid.desktop" ];
-      "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
+      "text/html" = [ "zen-twilight.desktop" ];
+      "x-scheme-handler/http" = [ "zen-twilight.desktop" ];
+      "x-scheme-handler/https" = [ "zen-twilight.desktop" ];
+      "image/png" = [ "com.interversehq.qView.desktop" ];
+      "image/svg" = [ "com.interversehq.qView.desktop" ];
+      "image/jpeg" = [ "com.interversehq.qView.desktop" ];
+      "image/gif" = [ "com.interversehq.qView.desktop" ];
+      "image/webp" = [ "com.interversehq.qView.desktop" ];
+      "video/mp4" = [ "mpv.desktop" ];
+      "video/avi" = [ "mpv.desktop" ];
+      "application/pdf" = [ "org.kde.okular.desktop" ];
+      "inode/directory" = [ "org.kde.dolphin.desktop" ];
     };
     mimeApps.defaultApplications = {
-      "text/html" = [ "zen.desktop" ];
-      "x-scheme-handler/http" = [ "zen.desktop" ];
-      "x-scheme-handler/https" = [ "zen.desktop" ];
-      "image/png" = [ "org.gnome.Loupe.desktop" ];
-      "image/svg" = [ "org.gnome.Loupe.desktop" ];
-      "image/jpeg" = [ "org.gnome.Loupe.desktop" ];
-      "image/gif" = [ "org.gnome.Loupe.desktop" ];
-      "image/webp" = [ "org.gnome.Loupe.desktop" ];
-      "video/mp4" = [ "io.github.celluloid_player.Celluloid.desktop" ];
-      "video/avi" = [ "io.github.celluloid_player.Celluloid.desktop" ];
-      "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
+      "text/html" = [ "zen-twilight.desktop" ];
+      "x-scheme-handler/http" = [ "zen-twilight.desktop" ];
+      "x-scheme-handler/https" = [ "zen-twilight.desktop" ];
+      "image/png" = [ "com.interversehq.qView.desktop" ];
+      "image/svg" = [ "com.interversehq.qView.desktop" ];
+      "image/jpeg" = [ "com.interversehq.qView.desktop" ];
+      "image/gif" = [ "com.interversehq.qView.desktop" ];
+      "image/webp" = [ "com.interversehq.qView.desktop" ];
+      "video/mp4" = [ "mpv.desktop" ];
+      "video/avi" = [ "mpv.desktop" ];
+      "application/pdf" = [ "org.kde.okular.desktop" ];
+      "inode/directory" = [ "org.kde.dolphin.desktop" ];
     };
   };
 }
