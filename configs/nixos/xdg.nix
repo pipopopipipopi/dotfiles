6cac28067a49ccfe3264256cac36b0096874918a
pipopo <puchi.maru.milk@gmail.com>
2026-04-19 11:25:39 +0900

    xdg-portal系をイジイジした

--- configs/nixos/xdg.nix
+++ configs/nixos/xdg.nix
@@ -2,16 +2,18 @@
   xdg.portal = {
     enable = true;
     extraPortals = [
+      pkgs.xdg-desktop-portal-wlr
+      pkgs.kdePackages.xdg-desktop-portal-kde
       pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal-gnome
     ];
     # config.common.default = "*";
     config = {
       common = {
-        default = ["gnome" "gtk"];
-        "org.freedesktop.impl.portal.ScreenCast" = "gnome";
-        "org.freedesktop.impl.portal.Screenshot" = "gnome";
-        "org.freedesktop.impl.portal.RemoteDesktop" = "gnome";
+        default = ["wlr" "kde" "gnome" "gtk"];
+        # "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+        # "org.freedesktop.impl.portal.Screenshot" = "gnome";
+        # "org.freedesktop.impl.portal.RemoteDesktop" = "gnome";
       };
     };
   };
