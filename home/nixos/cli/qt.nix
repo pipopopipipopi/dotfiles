07c9d2379a0321d026e3da9400da0d5a489a9c8f
pipopo <puchi.maru.milk@gmail.com>
2026-04-27 08:20:51 +0900

    edit qt settings

--- home/nixos/cli/qt.nix
+++ home/nixos/cli/qt.nix
@@ -2,12 +2,23 @@
   qt = {
     enable = true;
     platformTheme.name = "qtct";
-    style.name = "kvantum";
+    # style.name = "kvantum";
 
-    kvantum = {
-      enable = true;
-      themes = with pkgs; [ utterly-nord-plasma ];
-      settings.General.theme = "Utterly-Nord-Light-Solid";
-    };
+    # kvantum = {
+    #   enable = true;
+    #   themes = with pkgs; [ utterly-nord-plasma ];
+    #   settings.General.theme = "Utterly-Nord-Light-Solid";
+    # };
   };
+
+  xdg.configFile."qt5ct/qt5ct.conf".text = ''
+    [Appearance]
+    color_scheme_path=$HOME/.config/qt5ct/colors/matugen_theme.conf
+    custom_palette=true
+  '';
+  xdg.configFile."qt6ct/qt6ct.conf".text = ''
+    [Appearance]
+    color_scheme_path=$HOME/.config/qt6ct/colors/matugen_theme.conf
+    custom_palette=true
+  '';
 }
