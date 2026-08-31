4a3efeb715a08d882ac66c2a681afba15095dc2c
pipopo <puchi.maru.milk@gmail.com>
2026-09-01 08:52:56 +0900

    regreetを導入

--- hosts/lawine/nixos.nix
+++ hosts/lawine/nixos.nix
@@ -43,46 +43,48 @@
 
   programs.nix-ld.enable = true;
 
-  # services.greetd =
-  # let
-  #   minimumConfig = pkgs.writeText "minimum-config.kdl" ''
-  #     hotkey-overlay {
-  #       skip-at-startup
-  #     }
-  #     // https://github.com/rharish101/ReGreet?tab=readme-ov-file#startup-delays
-  #     environment {
-  #       GTK_USE_PORTAL "0"
-  #       GDK_DEBUG "no-portals"
-  #     }
-  #     spawn-sh-at-startup "${lib.getExe config.programs.regreet.package}; niri msg action quit --skip-confirmation"
-  #   '';
-  # in
-  # {
-  #   enable = true;
-  #   settings = {
-  #     default_session = {
-  #       command = "dbus-run-session ${lib.getExe config.programs.niri.package} --config ${minimumConfig}";
-  #       user = "greeter";
-  #     };
-  #   };
-  # };
-  
-  services.greetd = {
+  services.accounts-daemon.enable = true;
+  services.greetd =
+  let
+    minimumConfig = pkgs.writeText "minimum-config.kdl" ''
+      hotkey-overlay {
+        skip-at-startup
+      }
+      // https://github.com/rharish101/ReGreet?tab=readme-ov-file#startup-delays
+      environment {
+        GTK_USE_PORTAL "0"
+        GDK_DEBUG "no-portals"
+      }
+      spawn-sh-at-startup "${lib.getExe config.services.displayManager.regreet.package}; niri msg action quit --skip-confirmation"
+      // spawn-sh-at-startup "${lib.getExe config.services.displayManager.regreet.package} 2>/tmp/regreet.log"
+    '';
+  in
+  {
     enable = true;
     settings = {
-      # initial_session = {
-      #   command = "${pkgs.niri}/bin/niri-session";
-      #   user = "${username}";
-      # };
-      # default_session = {
-      #   command = "${pkgs.tuigreet}/bin/tuigreet --remember --remember-user-session --time --cmd ${pkgs.niri}/bin/niri-session";
-      #   user = "greeter";
-      # };
       default_session = {
-        # command = "${lib.getExe pkgs.tuigreet} --remember --remember-user-session --time --cmd ${config.programs.niri.package}/bin/niri-session";
-        command = "${lib.getExe pkgs.tuigreet} --time --remember --remember-user-session";
+        command = "dbus-run-session ${lib.getExe config.programs.niri.package} --config ${minimumConfig}";
         user = "greeter";
       };
     };
   };
+  
+  # services.greetd = {
+  #   enable = true;
+  #   settings = {
+  #     # initial_session = {
+  #     #   command = "${pkgs.niri}/bin/niri-session";
+  #     #   user = "${username}";
+  #     # };
+  #     # default_session = {
+  #     #   command = "${pkgs.tuigreet}/bin/tuigreet --remember --remember-user-session --time --cmd ${pkgs.niri}/bin/niri-session";
+  #     #   user = "greeter";
+  #     # };
+  #     default_session = {
+  #       # command = "${lib.getExe pkgs.tuigreet} --remember --remember-user-session --time --cmd ${config.programs.niri.package}/bin/niri-session";
+  #       command = "${lib.getExe pkgs.tuigreet} --time --remember --remember-user-session";
+  #       user = "greeter";
+  #     };
+  #   };
+  # };
 }
